a776155628922de636a4f113801e156425732759
guozhaohui <guozhaohui628@gmail.com>
2019-01-24 09:23:10 +0800

    lol

---
+++ UploadVideo.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.guozh.upload;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatus;
+import com.google.common.collect.Lists;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Upload a video to the authenticated user's channel. Use OAuth 2.0 to
+ * authorize the request. Note that you must add your video files to the
+ * project folder to upload them with this application.
+ *
+ * @author Jeremy Walker
+ */
+public class UploadVideo {
+
+    static String proxyHost = "127.0.0.1";
+    static String proxyPort = "1080";
+
+    private static YouTube youtube;
+
+    private static final String VIDEO_FILE_FORMAT = "video/*";
+
+    private static final String SAMPLE_VIDEO_FILENAME = "sample-video.mp4";
+
+    public static void main(String[] args) {
+
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+
+        // 对https也开启代理
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+
+
+        List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube.upload");
+
+        try {
+            Credential credential = Auth.authorize(scopes, "guozhtest");
+
+            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, credential).setApplicationName(
+                    "youtube-cmdline-uploadvideo-sample").build();
+
+            System.out.println("Uploading: " + SAMPLE_VIDEO_FILENAME);
+
+            Video videoObjectDefiningMetadata = new Video();
+
+            VideoStatus status = new VideoStatus();
+            status.setPrivacyStatus("public");
+            videoObjectDefiningMetadata.setStatus(status);
+
+            VideoSnippet snippet = new VideoSnippet();
+
+            Calendar cal = Calendar.getInstance();
+            snippet.setTitle("Test Upload via Java on " + cal.getTime());
+            snippet.setDescription(
+                    "Video uploaded via YouTube Data API V3 using the Java library " + "on " + cal.getTime());
+
+            List<String> tags = new ArrayList<String>();
+            tags.add("test");
+            tags.add("example");
+            tags.add("java");
+            tags.add("YouTube Data API V3");
+            tags.add("erase me");
+            snippet.setTags(tags);
+
+            videoObjectDefiningMetadata.setSnippet(snippet);
+
+            InputStreamContent mediaContent = new InputStreamContent(VIDEO_FILE_FORMAT,
+                    UploadVideo.class.getResourceAsStream("/sample-video.mp4"));
+
+
+            YouTube.Videos.Insert videoInsert = youtube.videos()
+                    .insert("snippet,statistics,status", videoObjectDefiningMetadata, mediaContent);
+
+            MediaHttpUploader uploader = videoInsert.getMediaHttpUploader();
+
+            uploader.setDirectUploadEnabled(false);
+
+            MediaHttpUploaderProgressListener progressListener = new MediaHttpUploaderProgressListener() {
+                public void progressChanged(MediaHttpUploader uploader) throws IOException {
+                    switch (uploader.getUploadState()) {
+                        case INITIATION_STARTED:
+                            System.out.println("Initiation Started");
+                            break;
+                        case INITIATION_COMPLETE:
+                            System.out.println("Initiation Completed");
+                            break;
+                        case MEDIA_IN_PROGRESS:
+                            System.out.println("Upload in progress");
+                            System.out.println("Upload percentage: " + uploader.getProgress());
+                            break;
+                        case MEDIA_COMPLETE:
+                            System.out.println("Upload Completed!");
+                            break;
+                        case NOT_STARTED:
+                            System.out.println("Upload Not Started!");
+                            break;
+                    }
+                }
+            };
+            uploader.setProgressListener(progressListener);
+
+            Video returnedVideo = videoInsert.execute();
+
+            System.out.println("\n================== Returned Video ==================\n");
+            System.out.println("  - Id: " + returnedVideo.getId());
+            System.out.println("  - Title: " + returnedVideo.getSnippet().getTitle());
+            System.out.println("  - Tags: " + returnedVideo.getSnippet().getTags());
+            System.out.println("  - Privacy Status: " + returnedVideo.getStatus().getPrivacyStatus());
+            System.out.println("  - Video Count: " + returnedVideo.getStatistics().getViewCount());
+
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("GoogleJsonResponseException code: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Throwable t) {
+            System.err.println("Throwable: " + t.getMessage());
+            t.printStackTrace();
+        }
+    }
+}
